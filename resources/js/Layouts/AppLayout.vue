ef5fe9d9abf9ca77bd718ee748fd088cf29124d2
wicaksuu <wicaksono.bayuaji.wba@gmail.com>
2023-06-07 22:03:36 +0700

    sad

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -44,7 +44,6 @@ const logout = () => {
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
                         <div class="flex">
-                            <!-- Logo -->
                             <div class="shrink-0 flex items-center">
                                 <Link :href="route('dashboard')">
                                     <ApplicationMark class="block h-9 w-auto" />
