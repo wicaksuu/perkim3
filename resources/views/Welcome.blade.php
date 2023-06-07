9a4f983733dc9e98b229fa109771b7a6d18b7697
wicaksuu <wicaksono.bayuaji.wba@gmail.com>
2023-06-07 20:38:53 +0700

    FSDFS

--- resources/views/Welcome.blade.php
+++ resources/views/Welcome.blade.php
@@ -1,10 +1,5 @@
 <!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!-->
 <html class="no-js" lang="en">
-<!--<![endif]-->
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -94,7 +89,7 @@
 <div class="dl-caption dl-border" data-animation="fade-in-left" data-delay="0s"></div>
 <div class="dl-caption big">
     <div class="inner-layer">
-        <div data-animation="fade-in-left" data-delay="2s">Si-Pedalrum</div>
+<div data-animation="fade-in-left" data-delay="2s">Si-PEDALRUM</div>
     </div>
 </div>
 <div class="dl-caption big">
@@ -105,7 +100,7 @@
 </div>
 <div class="dl-caption small">
     <div class="inner-layer">
-        <div data-animation="fade-in-left" data-delay="3s">Daerah</div>
+<div data-animation="fade-in-left" data-delay="3s"></div>
     </div>
 </div>
 <div class="dl-btn-group">
