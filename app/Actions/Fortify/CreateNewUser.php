5a9521f5d2794405834bcbe33dc9712d531e144d
wicaksuu <wicaksono.bayuaji.wba@gmail.com>
2023-06-11 14:54:13 +0700

    sda

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -29,7 +29,7 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
-            'role' => 'user',
+            'role' => 'User',
             'password' => Hash::make($input['password']),
         ]);
     }
